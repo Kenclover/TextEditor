2d9998eb3d15d85d4c04b5cf1f19d7237f72db1a
kenclover <kenclover90@gmail.com>
2026-02-08 22:10:39 +0530

    third commit

--- txteditor.c
+++ txteditor.c
@@ -6,9 +6,15 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <errno.h>
+#include <sys/ioctl.h>
 
 /***data***/
+struct editorConfig{
+int screenrows;
+int screencols;
 struct termios orig_termios;
+};
+struct editorConfig E;
 
 /***Terminal***/
 void die(const char *s){
@@ -19,13 +25,13 @@ void die(const char *s){
 }
 
 void disableRawMode() {
-	if ( tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1 ) die("tcsetattr");
+	if ( tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1 ) die("tcsetattr");
 }
 
 void enableRawMode() {
-	if ( tcgetattr(STDIN_FILENO,&orig_termios) == -1 ) die("tcgetattr");
+	if ( tcgetattr(STDIN_FILENO,&E.orig_termios) == -1 ) die("tcgetattr");
 	atexit(disableRawMode);
-	struct termios raw = orig_termios;
+	struct termios raw = E.orig_termios;
 	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
 	raw.c_oflag &= ~(OPOST);
 	raw.c_cflag |= (CS8);
@@ -42,6 +48,17 @@ char editorReadKey(){
 		if(nread == -1 && errno != EAGAIN) die("read");
 	}
 	return c;
+}
+
+int getWindowSize(int *rows, int *cols){
+	struct winsize ws;
+	if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws)== -1|| ws.ws_col ==0){
+	return -1;
+	} else {
+	*cols =ws.ws_col;
+	*rows =ws.ws_row;
+	return 0;
+	}
 }
 
 //OUTPUT
@@ -73,8 +90,13 @@ void editorProcessKeypress(){
 }
 
 /***init***/
+void initEditor(){
+	if(getWindowSize(&E.screenrows , &E.screencols) == -1) die("getWindowSize");
+}
+
 int main() {
 	enableRawMode();
+	initEditor();
 	while(1){
 	editorRefreshScreen();
 	editorProcessKeypress();
