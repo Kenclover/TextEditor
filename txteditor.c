493765da11c4554603a58882538fca6b2a14d90c
vivek <kenclover90@gmail.com>
2026-02-24 21:48:36 +0530

    fixed tabs and cursor

--- txteditor.c
+++ txteditor.c
@@ -38,6 +38,7 @@ char *render;
 
 struct editorConfig{
 int cx,cy;
+int rx;
 int rowoff;
 int coloff;
 int screenrows;
@@ -153,6 +154,17 @@ int getWindowSize(int *rows, int *cols){
 
 /***RoW Operations ***/
 
+int editorRowCxToRx(erow *row, int cx){
+        int rx =0;
+        int j;
+        for(j=0;j< cx;j++){
+          if(row->chars[j] == 't')
+                rx += (KILO_TAB_STOP -1)-(rx % KILO_TAB_STOP);
+          rx++;
+        }
+        return rx;
+}
+
 void editorUpdateRow(erow *row){
         int tabs = 0;
         int j;
@@ -228,17 +240,22 @@ void abFree(struct abuf *ab){
 
 //OUTPUT
 void editorScroll(){
+        E.rx = 0;
+        if(E.cy < E.numrows){
+         E.rx = editorRowCxToRx(&E.row[E.cy], E.cx);
+        }
+
         if(E.cy < E.rowoff){
          E.rowoff = E.cy;
         }
         if(E.cy > E.rowoff + E.screenrows){
          E.rowoff = E.cy - E.screenrows + 1;
         }
-        if(E.cx < E.coloff){
-         E.coloff = E.cx;
+        if(E.rx < E.coloff){
+         E.coloff = E.rx;
         }
-        if(E.cx >= E.coloff + E.screencols){
-         E.coloff = E.cx - E.screencols +1;
+        if(E.rx >= E.coloff + E.screencols){
+         E.coloff = E.rx - E.screencols +1;
         }
 }
 
@@ -285,7 +302,7 @@ void editorRefreshScreen(){
         editorDrawRow(&ab);
 
         char buf[32];
-        snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) +1, (E.cx - E.coloff) +1);
+        snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) +1, (E.rx - E.coloff) +1);
         abAppend(&ab, buf, strlen(buf));
         abAppend(&ab, "\x1b[H", 3);
         abAppend(&ab, "\x1b[?25h", 6);
@@ -370,6 +387,7 @@ void editorProcessKeypress(){
 void initEditor(){
         E.cx = 0;
         E.cy = 0;
+        E.rx = 0;
         E.rowoff = 0;
         E.coloff = 0;
         E.numrows = 0;
